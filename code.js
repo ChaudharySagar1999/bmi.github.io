bf21aef1ece54f914afcc56ac46759ae8eb92414
ChaudharySagar1999 <143334534+ChaudharySagar1999@users.noreply.github.com>
2023-09-02 12:43:29 +0530

    Bmi calculator

---
+++ code.js
@@ -0,0 +1,9 @@
+
+document.getElementById('btn').click,bmi
+function bmi(){
+    var h=document.getElementById('height').value;
+    var w=document.getElementById('weight').value;
+    var bmi=w/(h/100*h/100);
+    var total=bmi.toFixed(2);
+    document.getElementById('result').innerHTML="your bmi is"+total
+}
